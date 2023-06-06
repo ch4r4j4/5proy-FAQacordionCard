06f8d99c3c38be4d93065bca1807adca09aca46a
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-06-05 22:52:57 -0500

    adding ht e class for the item active

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 /// tengo que revisar l del primer dia de github 50 days y agreggar las funcionalidades a github
+const respuestas = document.querySelectorAll('.answer');
+
+respuestas.forEach(respuesta =>{
+    respuesta.addEventListener('click',()=>{
+        removeActiveClasses()
+        respuesta.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    respuestas.forEach(respuesta => {
+        respuesta.classList.remove('active')
+    })
+}
+// literal me bloquee en el desarrollo de esta actividad tengo que ver manejo del dom
