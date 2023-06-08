28417777c25e44d87ef7df543e83ad9aa3dae02f
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-06-07 22:37:02 -0500

    cerca a terminar la funcionalidad de JS

--- js/app.js
+++ js/app.js
@@ -1,16 +1,30 @@
 /// tengo que revisar l del primer dia de github 50 days y agreggar las funcionalidades a github
-const respuestas = document.querySelectorAll('.answer');
+/*const respuestas = document.querySelectorAll('.item');
 
-respuestas.forEach(respuesta =>{
-    respuesta.addEventListener('click',()=>{
+/*respuestas.forEach(item =>{
+    item.addEventListener('click',()=>{
         removeActiveClasses()
         respuesta.classList.add('active')
     })
 })
 
 function removeActiveClasses(){
-    respuestas.forEach(respuesta => {
+    respuestas.forEach(item => {
         respuesta.classList.remove('active')
     })
-}
+}*/
 // literal me bloquee en el desarrollo de esta actividad tengo que ver manejo del dom
+"use strict";
+const allQuestions = document.querySelectorAll(".item");
+
+allQuestions.forEach(function(item){
+    item.addEventListener("click", function(){
+        const outputAnswer = item.querySelector(".answer");
+        const question = item.querySelector(".question");
+        const arrow = item.querySelector(".arrow");
+
+        outputAnswer.classList.add("active");
+        question.classList.toggle("active");
+        arrow.classList.toggle("rotate");
+    });
+});
