66e2e3bc5663281141dfe7300bfb22fd0a6fca6d
Paul Charaja Torres <68834265+ch4r4j4@users.noreply.github.com>
2023-06-08 23:37:44 -0500

    styles in project

--- js/app.js
+++ js/app.js
@@ -24,20 +24,4 @@ function removeActiveClasses(){
         flecha.classList.remove("rotate");
     })
 }
-
-
-// literal me bloquee en el desarrollo de esta actividad tengo que ver manejo del dom
-/*"use strict";
-const allQuestions = document.querySelectorAll(".item");
-
-allQuestions.forEach(function(item){
-    item.addEventListener("click", function(){
-        const outputAnswer = item.querySelector(".answer");
-        const question = item.querySelector(".question");
-        const arrow = item.querySelector(".arrow");
-
-        outputAnswer.classList.add("active");
-        question.classList.toggle("active");
-        arrow.classList.toggle("rotate");
-    });
-});*/
+///podemos solucionarlo con un if averiguar como utilizar esta condicion;
